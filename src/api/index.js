bad046c0446a89cc6b83c657baa0c1ca84008807
26862-李继军 <jijun.li@hand-china.com>
2019-12-30 11:17:56 +0800

    ljj

--- src/api/index.js
+++ src/api/index.js
@@ -5,6 +5,8 @@
  * 每个函数的返回值都是promise
  */
 import ajax from './ajax'
+import jsonp from 'jsonp'
+import { message } from 'antd'
 
 const BASE = ''
 //登录 
@@ -15,3 +17,23 @@ export const reqLogin = (username,password) => ajax(BASE + '/login', {username,p
 
 //添加用户
 export const reqAddUser = (user) => ajax(BASE + '/manage/user/add', user, 'POST')
+
+//json请求的接口数据
+export const reqWeather = (city) => {
+  return new Promise((resolve,reject)=>{
+    const url =  `http://wthrcdn.etouch.cn/weather_mini?city=${city}`
+    // console.log(url)
+    //发送jsonp请求
+    jsonp(url,{},(err,data)=>{
+      console.log('jsonp()',err,data)
+      if(!err && data.status === 1000){
+        //去除需要的数据
+        const weather = data.data.forecast[0].type
+        resolve(weather)
+      }else{
+        message.error('获取天气信息失败')
+      }
+    })
+  }) 
+}
+// reqWeather('北京')
